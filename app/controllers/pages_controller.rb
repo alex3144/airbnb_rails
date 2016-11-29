76794d8ffb67d2a4982cf5a38bd68951b15bb6f1
Mael donnard <mael.donnard@gmail.com>
2016-11-29 15:23:49 +0100

    modify the restricted access

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :home]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
   end
