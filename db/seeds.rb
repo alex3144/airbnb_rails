524ece4f336d6bfdf63bca840a82fd4f617e801a
remy lecostey <remy.lecostey@gmail.com>
2016-11-29 12:20:19 +0100

    Add new features for Bike Index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,12 @@ Booking.destroy_all
 Bike.destroy_all
 User.destroy_all
 
-owner = User.create!(first_name: "bob", email: "bob@bob.com", password: "123456", password_confirmation: "123456")
-renter = User.create!(first_name: "alice", email: "alicce@alice.com", password: "123456", password_confirmation: "123456")
-bike = Bike.create(brand:"peugeot", user: owner)
-booking =  Booking.new
-booking.user = renter
-booking.bike = bike
-booking.save!
+user1 = User.create!(first_name:"Bob", email:"test@free.fr", password:"hzbdizhdb", password_confirmation:"hzbdizhdb")
+# user1.save
 
-
+bike1 = Bike.create!(city:"Nantes", brand:"Peugeot", title:"Vélo de collection", detail:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", user_id: 3)
+bike2 = Bike.create!(city:"Bordeaux", brand:"Scott", title:"VTT 29\" semi rigide", detail:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", user_id: 3)
+bike3 = Bike.create!(city:"Amsterdam", brand:"Specialized", title:"VTT 29\" tout supendu", detail:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", user_id: 3)
+bike4 = Bike.create!(city:"Cherbourg", brand:"Commencal", title:"VTT 29\" semi rigide", detail:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", user_id: 3)
+bike5 = Bike.create!(city:"Paris", brand:"Canyon", title:"VTT 29\" semi rigide", detail:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", user_id: 3)
+bike6 = Bike.create!(city:"Nantes", brand:"Peugeot", title:"VTT 29\" semi rigide", detail:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.", user_id: 3)
