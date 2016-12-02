d7b5a69bc8647f382695de4098c2900ba09d28e7
Matthieu Le Berre <matthieu.leberre@gmail.com>
2016-12-02 14:04:34 +0100

    form designed + home +card

--- app/models/bike.rb
+++ app/models/bike.rb
@@ -2,5 +2,7 @@ class Bike < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_attachments :photos, maximum: 3
-
+  validates :photos, presence: true
+  validates :title, presence: true
+  validates :city, presence:  true
 end
